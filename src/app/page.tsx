6c8df057f8ce6c42e4acee6420b62025deeb5c19
vlk2022 <volodymyrkostiuk2021@gmail.com>
2024-04-15 23:16:36 +0300

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,12 @@
 import styles from "./page.module.css";
-import course from './course.json';
-import {Lesson} from "./lesson";
 
 
-async function getAllCourses(): Promise<typeof course> {
-    return new Promise((resolve) => {
-        setTimeout(() => resolve(course), 1000);
-    })
-}
+export default function Home() {
 
-
-export default async function Home() {
-    const courses = await getAllCourses();
 
     return (
         <main className={styles.main}>
-            <h2>TEST</h2>
-            <ul>
-                {courses.lessons.map(lesson => {
-                    return <li key={lesson.name}>
-                        <Lesson title={lesson.title}/>
-                    </li>
-                })}
-            </ul>
+
         </main>
     );
 }
