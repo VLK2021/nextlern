2d7e08e9f32db31c0df24034718419de1a0f92bf
vlk2022 <volodymyrkostiuk2021@gmail.com>
2024-04-15 23:07:18 +0300

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,7 +16,6 @@ export default async function Home() {
     return (
         <main className={styles.main}>
             <h2>TEST</h2>
-
             <ul>
                 {courses.lessons.map(lesson => {
                     return <li key={lesson.name}>
